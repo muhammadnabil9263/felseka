27b4f0b5b2e25f02e0147535b7c57b088e7ea95d
muhammadnabil9263 <muhammadnabeel9236@gmail.com>
2020-11-03 00:23:05 +0200

    first commit

---
+++ accounts/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from .views import RegisterAPI
+from knox import views as knox_views
+from .views import LoginAPI
+
+urlpatterns = [
+    path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+
+]
